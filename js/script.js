fcb302ec5f64dc65a94a5bef415cefec46f657ad
OlgaSamoilenko <70361687+OlgaSamoilenko@users.noreply.github.com>
2021-04-15 17:32:05 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
     $('input[name=phone]').mask("+7 (999) 99-99-999");
-  });
+});
+$('.catalog-item__link').each(function(i) {
+    $(this).on('click', function(e) {
+        e.preventDefuit();
+        $('.catalog-item__content ').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').toggleClass('catalog-item__list_active');
+    })
+
+});
+$('.catalog-item__back').each(function(i) {
+    $(this).on('click', function(e) {
+        e.preventDefuit();
+        $('.catalog-item__content ').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').toggleClass('catalog-item__list_active');
+    })
+
+});
